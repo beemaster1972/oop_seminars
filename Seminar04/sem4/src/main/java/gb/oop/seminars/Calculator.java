c50cb322146847db81eb8179785c99c845df1ea4
beemaster1972 <ugor.finno@yandex.ru>
2024-02-05 17:19:22 +0500

    Add seminar 4

---
+++ Seminar04/sem4/src/main/java/gb/oop/seminars/Calculator.java
@@ -0,0 +1,24 @@
+/*
+3)	Написать класс калькулятор, принимающий List целочисленных значений,
+возвращающий сумму элементов коллекции (метод sum)
+ */
+package gb.oop.seminars;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Calculator<N extends Number> {
+    private List<N> numbers;
+
+    public Double sum() {
+        return numbers.stream().mapToDouble(Number::doubleValue).sum();
+    }
+}
+
---
+++ Seminar04/sem4/src/main/java/gb/oop/seminars/Main.java
@@ -0,0 +1,29 @@
+package gb.oop.seminars;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        GenBox<Integer> box1 = new GenBox<>(10);
+//        GenBox<Integer> box2 = new GenBox<>(20);
+//        GenBox<Double> box3 = new GenBox<>(100.0);
+//        int sum = box1.getObj() + box2.getObj();
+//        System.out.println(sum);
+//        Double sum2 = box3.getObj()+box2.getObj();
+//        System.out.println(sum2);
+        BoxWithNumbers<Double> list = new BoxWithNumbers<>(new Double[]{1.0, 2.0, 3.0, 4.0, 5.0, 6.0});
+        System.out.println(list.average());
+        BoxWithNumbers<Integer> intList = new BoxWithNumbers<>(new Integer[]{1, 2, 3, 4, 5, 6,7,8});
+        System.out.println(intList.average());
+        System.out.println(list.compareAverage(intList));
+        Calculator<Integer> calc = new Calculator<>();
+        for (int i = 0; i < 100 ; i++) {
+            calc.getNumbers().add(i);
+
+        }
+        System.out.println(calc.sum());
+    }
+}
